d1e07d10940dcc748f83b32832b4ed35d9aa3a08
Ju-ve Chan <juve.chankasemporn@digipen.edu>
2025-11-21 10:34:59 -0800

    remove shadows

--- HipWhipGame/Assets/Scripts/AssetBundle/BundleLoader.cs
+++ HipWhipGame/Assets/Scripts/AssetBundle/BundleLoader.cs
@@ -5,17 +5,25 @@ using UnityEngine.SceneManagement;
 
 public class BundleLoader : MonoBehaviour
 {
-    private AssetBundle stageBundle;
+    private void Start() 
+    {
+#if UNITY_EDITOR_64 || UNITY_STANDALONE_WIN
+        StartCoroutine(LoadAssetBundlePC());
+        SceneManager.LoadScene("ControllerAssignment");
+#elif UNITY_SWITCH
 
-    public IEnumerator Start()
+#endif
+    }
+
+    public IEnumerator LoadAssetBundlePC()
     {
         string path = Path.Combine(Application.streamingAssetsPath + "/StandaloneWindows64", "stage");
 
         var bundleLoadRequest = AssetBundle.LoadFromFileAsync(path);
         yield return bundleLoadRequest;
 
-        stageBundle = bundleLoadRequest.assetBundle;
 
-        SceneManager.LoadScene("ControllerAssignment");
     }
+
+
 }
